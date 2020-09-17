5df9dc5d513ced83d58947bd8a5502fd29b3e041
KaneLeung <kk.xiaolan@gmail.com>
2020-09-18 01:07:53 +0800

    :pushpin:*更新Nuget版本为1.0.9

--- Src/系统类/WinHotKey.cs
+++ Src/系统类/WinHotKey.cs
@@ -17,13 +17,12 @@
 *******************************************************************
 -----------------------------------------------------------------*/
 #endregion
-using Kane.Extension;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Forms;
+using Kane.Extension;
 
 namespace Kane.WinForm
 {
